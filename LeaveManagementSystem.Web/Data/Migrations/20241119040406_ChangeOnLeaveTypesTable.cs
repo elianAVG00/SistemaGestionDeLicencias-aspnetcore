dcd9553ffd48d5ba11a810972a8c6374798f6cd3
elianAVG00 <elian.avgonzalez@hotmail.com>
2024-11-19 01:07:28 -0300

    Creo LeaveType class. su migracion, su tabla usando ef

---
+++ LeaveManagementSystem.Web/Data/Migrations/20241119040406_ChangeOnLeaveTypesTable.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace LeaveManagementSystem.Web.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class ChangeOnLeaveTypesTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "NumberODays",
+                table: "LeaveTypes",
+                newName: "NumberOfDays");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "NumberOfDays",
+                table: "LeaveTypes",
+                newName: "NumberODays");
+        }
+    }
+}
